5be4ea77496dc004280cd037998070317aa4088a
tauseef-ameen <tauseef.125002@gmail.com>
2024-08-24 19:01:06 +0200

    pushed containerize solution

--- src/main/java/com/example/medium/containerize/ContainerizeApplication.java
+++ src/main/java/com/example/medium/containerize/ContainerizeApplication.java
@@ -11,25 +11,6 @@ import java.util.List;
 public class ContainerizeApplication {
 
 	public static void main(String[] args) {
-		// Original object
-		List<String> hobbies = new ArrayList<>();
-		hobbies.add("Running");
-		hobbies.add("swimming");
-
-		Person originalPerson = new Person("John", hobbies);
-
-		// Shallow copy using copy constructor
-		Person shallowCopyPerson = new Person(originalPerson);
-
-		// Modifying the shallow copy
-		shallowCopyPerson.hobbies.add("playing");
-
-		// Original object remains unchanged
-		System.out.println("Original util.Person: " + originalPerson.hobbies);
-		System.out.println("Shallow Copy util.Person: " + shallowCopyPerson.hobbies);
-
-
-
 		SpringApplication.run(ContainerizeApplication.class, args);
 	}
 
