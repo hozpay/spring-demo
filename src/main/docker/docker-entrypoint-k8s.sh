08220e32ae896ec5ffe31c7f480105f1592f0ce0
tauseef-ameen <tauseef.125002@gmail.com>
2024-09-01 20:46:35 +0200

    added docker-entrypoint-k8s.sh file to support 2 scripts in single image

--- src/main/docker/docker-entrypoint-k8s.sh
+++ src/main/docker/docker-entrypoint-k8s.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 set -e
 
-echo 'Starting default Spring Boot App'
+echo 'Starting customised Spring Boot App for kubernetes via commands'
 
-if [ "$DEBUG" = true ]; then
-  printf "Running the application in debug mode\n"
-  JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:$DEBUG_PORT"
-fi
-mkdir -p tauseef
+mkdir -p kubernetes
 # Enables application to take PID 1 and receive SIGTERM sent by Docker stop command.
 # See here https://docs.docker.com/engine/reference/builder/#/entrypoint
 exec java $JAVA_OPTS \
